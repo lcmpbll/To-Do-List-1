70fba2b925d807b281e124f4c683a460d545bb57
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-24 20:52:43 -0700

    Update startup with sql database path

--- ToDoList.Solutions/ToDoList/Startup.cs
+++ ToDoList.Solutions/ToDoList/Startup.cs
@@ -16,6 +16,11 @@ namespace ToDoList
           .AddEnvironmentVariables();
       Configuration = builder.Build();
     }
+
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+  }
   
   public IConfigurationRoot Configuration { get; }
 
