8e07972cbd947342cabfdbe2d487bead7c157056
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-25 15:17:58 -0700

    Update to us entity framework

--- ToDoList.Solutions/ToDoList/Startup.cs
+++ ToDoList.Solutions/ToDoList/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameWorkCore;
 
 
 namespace ToDoList
@@ -17,11 +18,14 @@ namespace ToDoList
       Configuration = builder.Build();
     }
   
-    public IConfigurationRoot Configuration { get; }
+    public IConfigurationRoot Configuration { get; set;  }
 
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+      services.AddEntityFrameworkMmySql()
+        .AddDbContext<ToDoListContext>(options => options
+        .UsemySql(configurations["ConnectionStraings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
     public void Configure(IApplicationBuilder app)
