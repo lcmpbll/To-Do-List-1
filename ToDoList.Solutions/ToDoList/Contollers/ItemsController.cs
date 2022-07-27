ac594ea9de42e25984867a103065ccf2744096c1
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-27 10:11:52 -0700

    use view bag in edit and create views

--- ToDoList.Solutions/ToDoList/Contollers/ItemsController.cs
+++ ToDoList.Solutions/ToDoList/Contollers/ItemsController.cs
@@ -1,35 +1,75 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspnetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
     {
-      Category category =Category.Find(categoryId);
-      return View(category);
+      _db = db;
+    }
+   
+    public ActionResult Index()
+    {
+      List<Item>model = _db.Items.Include(item => item.Category).ToList();
+      //ViewBag.PageTitle = "View All Items"
+      return View(model);
+    }
+      public ActionResult Create()
+    {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
     }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
+    [HttpPost]
+    public ActionResult Create(Item item)
     {
-      Item.ClearAll();
-      return View();
-    }    
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId , int itemId )
+    public ActionResult Details(int id)
     {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary <string,object> model = new Dictionary<string,object>();
-      model.Add("item", item);
-      model.Add("category" , category);
-      return View(model);
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+  
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => itemm.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Entry(item).State = EntityStateModified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Itemms.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
